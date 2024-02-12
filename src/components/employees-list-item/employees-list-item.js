8ecf0fab93db63857d6815a13972e2df8156a3ef
dmknpd <soupuabt@gmail.com>
2024-02-12 13:22:54 +0200

    second commit

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,25 +1,42 @@
 import "./employees-list-item.css";
 
-const EmployeesListItem = ({ name, salary, increase }) => {
+const EmployeesListItem = (props) => {
+  const { name, salary, onDelete, onToggleProp, increase, rise } = props;
+
   let className = "list-group-item d-flex justify-content-between";
   if (increase) {
     className += " increase";
   }
 
+  if (rise) {
+    className += " like";
+  }
+
   return (
     <li className={className}>
-      <span className="list-group-item-label">{name}</span>
+      <span
+        onClick={onToggleProp}
+        data-toggle="rise"
+        className="list-group-item-label"
+      >
+        {name}
+      </span>
       <input
         type="text"
         defaultValue={salary + "$"}
         className="list-group-item-input"
       />
       <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm">
+        <button
+          onClick={onToggleProp}
+          data-toggle="increase"
+          type="button"
+          className="btn-cookie btn-sm"
+        >
           <i className="fas fa-cookie"></i>
         </button>
 
-        <button type="button" className="btn-trash btn-sm">
+        <button onClick={onDelete} type="button" className="btn-trash btn-sm">
           <i className="fas fa-trash"></i>
         </button>
         <i className="fas fa-star"></i>
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,9 +1,19 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete, onToggleProp }) => {
   const elements = data.map((item) => {
-    return <EmployeesListItem {...item} />;
+    const { id, ...itemProps } = item;
+    return (
+      <EmployeesListItem
+        key={id}
+        {...itemProps}
+        onDelete={() => onDelete(id)}
+        onToggleProp={(e) =>
+          onToggleProp(id, e.currentTarget.getAttribute("data-toggle"))
+        }
+      />
+    );
   });
 
   return <ul className="app-list list-group">{elements}</ul>;
